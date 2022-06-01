95c9b87f2876f97106a540165434af90794ffb4d
Tahiba Rahman <105088285+rahmantahiba@users.noreply.github.com>
2022-06-01 17:42:33 -0400

    Going to have to practice creating these shapes so writing down is necessary

---
+++ square.py
@@ -0,0 +1,13 @@
+#Objective: Create a square 
+
+import turtle
+
+t = turtle.Turtle()
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
---
+++ triangle.py
@@ -0,0 +1,16 @@
+#Creating triangle and using the fill command 
+
+import turtle
+
+t = turtle.Turtle()
+
+#The default color is black
+
+t.begin_fill()
+t.color('pink')
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.end_fill()
