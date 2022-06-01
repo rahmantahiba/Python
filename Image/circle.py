447a235d7b14ac3eaae2125ade2d5daac2923298
Tahiba Rahman <105088285+rahmantahiba@users.noreply.github.com>
2022-06-01 17:40:50 -0400

    How to create a circle by shorthanding

---
+++ Image/circle.py
@@ -0,0 +1,10 @@
+import turtle 
+
+t = turtle.Turtle()
+
+#How to reduce the workload
+
+t.pen(pencolor="purple", fillcolor="pink", pensize=9, speed=5)
+t.begin_fill()
+t.circle(90) #90 is the size of the circle
+t.end_fill()
