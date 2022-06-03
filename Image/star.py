57ecab779e1317d8a58900a8d8fbc209dd75e370
Tahiba Rahman <105088285+rahmantahiba@users.noreply.github.com>
2022-06-03 01:09:55 -0400

    Creation of star shape successful

---
+++ Image/star.py
@@ -0,0 +1,19 @@
+import turtle
+
+star = turtle.Turtle()
+
+turtle.title("The Star Image")
+star.begin_fill()
+star.pencolor("magenta")
+star.fillcolor("yellow")
+star.right(75)
+star.forward(100)
+star.right(144)
+star.forward(100)
+star.right(144)
+star.forward(100)
+star.right(144)
+star.forward(100)
+star.right(144)
+star.forward(100)
+star.end_fill()
