88c471283b48a7b482fcea7f7d3179280c9fef19
Tahiba Rahman <105088285+rahmantahiba@users.noreply.github.com>
2022-06-09 17:02:14 -0400

    Need more practice on while loop will solve more problems later on

--- Loop/while.py
+++ Loop/while.py
@@ -1,5 +1,8 @@
 #Printing 1 through 10 using while loop 
 
+from re import S
+
+
 def one_ten():
     i = 0
     while i <= 10:
@@ -16,8 +19,37 @@ def multiple():
         print(start, "*", user, "=", operate)
         start += 1
 
+#Review 
+
+def square():
+    initial = 0
+    while initial <= 10:
+        s = initial**2
+        initial += 1
+        print("The exponent by 2 is:", s)
+
+def input_square():
+    initial = 0
+    i = int(input("Enter the number for the exponent: "))
+    while initial <= 10:
+        solve = initial**i
+        initial += 1 #Do not increment solve
+        print(initial,"^", i,"=", solve)
+
+def odd_even():
+    s = 0
+    while s <= 10:
+        if(s % 2 == 1):
+            print(s, "Odd")
+        else:
+            print(s, "Even")
+        s += 1
+
 def main():
     one_ten()
     multiple()
+    square()
+    input_square()
+    odd_even()
 
 main()
