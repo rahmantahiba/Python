a0068de3f2743e14b51d4be2c2cba9e7d3c71730
Tahiba Rahman <105088285+rahmantahiba@users.noreply.github.com>
2022-06-30 17:23:03 -0400

    Iterating through list is currently in progress

---
+++ List/iterate_list.py
@@ -0,0 +1,21 @@
+def membership():
+
+    m = [1,2,3,4,5]
+    if(2 in m):
+        print("True")
+    else:
+        print("False")
+
+#Printing a list through iteration 
+
+def i_list():
+
+    list = ["J", "K", "L"]
+    for i in list:
+        print(i)
+
+def main():
+    membership()
+    i_list()
+
+main()
