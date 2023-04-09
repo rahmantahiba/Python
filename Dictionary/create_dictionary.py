06739166374a264c433b99d7dcdfcf3c0887390e
Tahiba Rahman <105088285+rahmantahiba@users.noreply.github.com>
2023-04-08 22:54:17 -0400

    Create create_dictionary.py

---
+++ Dictionary/create_dictionary.py
@@ -0,0 +1,16 @@
+#how to list items using dictionaries 
+
+def create_dict():
+    anime_dict = {
+        "Anime Title": "Spy x Family",
+        "Release Date": "April 9, 2022",
+        "Episodes": 12
+
+    }
+
+    print(anime_dict)
+
+def main():
+    create_dict()
+
+main()
